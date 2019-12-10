399fe7886f8aa0b91ecae9af1daf6842220c7ee7
Raul Leal <raul.leal@cuatroochenta.com>
2019-12-10 20:38:33 +0100

    trasteando

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,117 @@
 #include <Arduino.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* mqtt_server = "34.226.229.35"; //1883
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    //digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    //digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266agdflkjadsy";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
+  pinMode(A0, INPUT);
   pinMode(D0, OUTPUT);
   pinMode(D1, INPUT);
+
+      //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+    //reset saved settings
+    
+    //wifiManager.resetSettings();
+    
+    //set custom ip for portal
+    //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+    //fetches ssid and pass from eeprom and tries to connect
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP"
+    //and goes into a blocking loop awaiting configuration
+    wifiManager.autoConnect("EquipoLuces");
+    //or use this for auto generated name ESP + ChipID
+    //wifiManager.autoConnect();
+
+    
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
 
 }
 
 void loop() {
-  Serial.println("Hola mundo");
 
-  digitalWrite(D0, HIGH);
-  int value = digitalRead(D1);
+/* if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+*/
+  int sensorLuz = analogRead(A0);
+
+
+  if (sensorLuz < 250) {
+digitalWrite(D0, HIGH);
+  } else {
+    digitalWrite(D0, LOW);
+  }
+
+  
 
   delay(1000);
-  // put your main code here, to run repeatedly:
+
+
+;  // put your main code here, to run repeatedly:
 }
